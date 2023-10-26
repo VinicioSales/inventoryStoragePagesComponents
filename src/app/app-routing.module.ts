aab05bb214acca8af9b9f847695f33e25a4e9241
VinicioSales <vinicio.santtana@gmail.com>
2023-10-26 13:40:03 -0300

    feat: Criado component login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { RegistroComponent } from './components/registro/registro.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {path:'registro', component: RegistroComponent},
+  {path:'login', component: LoginComponent},
 ];
 
 @NgModule({
