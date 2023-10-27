77b2e1eead885d3bed61d906cde6fea4b8cc1886
VinicioSales <vinicio.santtana@gmail.com>
2023-10-27 16:59:23 -0300

    configurando testes unitarios

--- src/app/components/input-dropdown/input-dropdown.component.spec.ts
+++ src/app/components/input-dropdown/input-dropdown.component.spec.ts
@@ -1,21 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { InputDropdownComponent } from './input-dropdown.component';
+import { TemaService } from '../../services/tema.service';
+import { of } from 'rxjs';
 
 describe('InputDropdownComponent', () => {
   let component: InputDropdownComponent;
   let fixture: ComponentFixture<InputDropdownComponent>;
+  let temaServiceSpy: jasmine.SpyObj<TemaService>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [InputDropdownComponent]
+  beforeEach(async () => {
+    // Crie um espião para o serviço TemaService
+    temaServiceSpy = jasmine.createSpyObj('TemaService', [], {
+      temaEscuroLigado$: of(false) // valor padrão
     });
+
+    await TestBed.configureTestingModule({
+      declarations: [InputDropdownComponent],
+      providers: [
+        { provide: TemaService, useValue: temaServiceSpy }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(InputDropdownComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('Deve fechar o dropdown ao clicar fora do container', () => {
+    component.mostrarDropdown = true;
+  const event = { target: document.createElement('div') };
+  component.handleClick(event as any);
+  expect(component.mostrarDropdown).toBe(false);
+});
 });
