3846390c0ee317a98da6b47ae95a31064328cab1
VinicioSales <vinicio.santtana@gmail.com>
2023-10-30 16:33:29 -0300

    feat: Add teste para inputDropdown

---
+++ notes.md
@@ -0,0 +1,15 @@
+1. Logo BF nas telas
+2. Colocar a logo da parceiro mais discreto
+3. Adicionar os títulos das telas de rotinas (OK)
+4. Remover a data de entrega da coluna de produtos soliciando (OK)
+5. Identificar o codigo da solicitação (OK)
+6. Pesquisar a solicitação por código (OK)
+7. Botao de continuar na tela de devolucao fixo (oK)
+8. Adicionar o codigo do produto (OK)
+9. Adicionar botao de selecionar todos na aprovacao parcial (OK)
+10. Almoxarifado poder solicitar a devolução
+11. Adicionar opção de pesquisa de usuario na tela de devolução (OK)
+	a. Caso for master o input fique ativo
+	b. Caso usuario comum, input fica inativo
+        
+
--- src/app/components/input-dropdown/input-dropdown.component.spec.ts
+++ src/app/components/input-dropdown/input-dropdown.component.spec.ts
@@ -28,10 +28,35 @@ describe('InputDropdownComponent', () => {
     fixture.detectChanges();
   });
 
+  //NOTE - handleClick
   it('Deve fechar o dropdown ao clicar fora do container', () => {
     component.mostrarDropdown = true;
     const event = { target: document.createElement('div') };
     component.handleClick(event as any);
     expect(component.mostrarDropdown).toBe(false);
   });
+
+  it('Não deve fechar o dropdown ao clicar dentro do container', () => {
+    component.mostrarDropdown = true;
+    const event = { target: component.containerRef.nativeElement };
+    component.handleClick(event as any);
+    expect(component.mostrarDropdown).toBe(true);
+  });
+  
+  it('Deve chamar handleBorderRadius ao fechar o dropdown', () => {
+    const handleBorderRadiusSpy = spyOn(component, 'handleBorderRadius');
+    component.mostrarDropdown = true;
+    const event = { target: document.createElement('div') };
+    component.handleClick(event as any);
+    expect(handleBorderRadiusSpy).toHaveBeenCalled();
+  });
+  
+  it('Não deve chamar handleBorderRadius se o dropdown não fechar', () => {
+    const handleBorderRadiusSpy = spyOn(component, 'handleBorderRadius');
+    component.mostrarDropdown = true;
+    const event = { target: component.containerRef.nativeElement };
+    component.handleClick(event as any);
+    expect(handleBorderRadiusSpy).not.toHaveBeenCalled();
+  });
+
 });
