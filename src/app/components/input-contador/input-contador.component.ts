6ced3667697b072e451593d6d14547586a75ca7f
VinicioSales <vinicio.santtana@gmail.com>
2023-10-31 17:18:16 -0300

    feat: Removido variaveis

--- src/app/components/input-contador/input-contador.component.ts
+++ src/app/components/input-contador/input-contador.component.ts
@@ -1,8 +1,3 @@
-// import { Subscription } from 'rxjs';
-// import { TemaService } from 'src/app/services/tema.service'
-// import { ImagemService } from 'src/app/services/imagem.service';
-// import { Component, Input, OnInit, OnDestroy } from '@angular/core';
-
 import { Subscription } from 'rxjs';
 import { TemaService } from '../../services/tema.service';
 import { ImagemService } from 'src/app/services/imagem.service';
@@ -15,76 +10,27 @@ import { Component, ElementRef, Input, Output, EventEmitter, ViewChild, OnInit, 
   styleUrls: ['./input-contador.component.css']
 })
 export class InputContadorComponent implements OnInit, OnDestroy {
-  // ngOnInit(): void {
-  //   this.subscription.add(
-  //     this.temaService.temaEscuroLigado$.subscribe(() => {
-  //       this.atualizarImg();
-  //     })
-  //   );
-  // }
-  // private subscription = new Subscription();
-
-  // @Input() width: string = '293px';
-  // @Input() height: string = '50px';
-  // @Input() itens: string[] = [];
-  // @Input() placeholder: string = 'input';
-
-  // imgSrc?: string;
-  // imgTemaEscuro: string = 'assets/img/dropdown-dark-mode.png';
-  // imgTemaClaro: string = 'assets/img/dropdown-light-mode.png';
-
-  // //NOTE - constructor
-  // constructor(private temaService: TemaService, private imagemService: ImagemService) {
-  //   this.temaService.temaEscuroLigado$.subscribe(() => {
-  //     this.atualizarImg();
-  //   });
-  
-  //   this.atualizarImg();
-  // }
-
-  // ngOnDestroy(): void {
-  //   this.subscription.unsubscribe();
-  // }
-
-  // //NOTE - atualizarImg
-  // atualizarImg() {
-  //   this.imgSrc = this.imagemService.atualizarImg(this.imgTemaClaro, this.imgTemaEscuro);
-  // }
-
   private subscription = new Subscription();
   
   // Inputs
   @Input() width: string = '293px';
   @Input() height: string = '50px';
-  @Input() itens: string[] = [];
   @Input() placeholder: string = 'input';
 
   // Variáveis
   imgSrc?: string;
-  itemSelecionado: string = '';
-  textoPesquisado: string = '';
-  borderRadius: string = '10px';
   mostrarDropdown: boolean = false;
-  itensFiltrados?: string[];
 
   // Imagens para os temas claro e escuro
   imgTemaEscuro: string = 'assets/img/dropdown-dark-mode.png';
   imgTemaClaro: string = 'assets/img/dropdown-light-mode.png';
 
-  // Outputs
-  @Output() botaoClicado = new EventEmitter<void>();
-  @Output() itemSelecionadoChange = new EventEmitter<string>();
-
-  // Viewchild
-  @ViewChild('containerRef') containerRef!: ElementRef;
-  
   constructor(private temaService: TemaService, private imagemService: ImagemService) {
     this.atualizarImg(); // Atualizar a imagem quando o componente é criado
   }
 
   //NOTE - ngOnInit
   ngOnInit(): void {
-    this.itensFiltrados = [...this.itens];
     this.subscription.add(
       this.temaService.temaEscuroLigado$.subscribe(() => {
         this.atualizarImg();
