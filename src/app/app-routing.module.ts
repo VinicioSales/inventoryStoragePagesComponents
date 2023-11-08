d5f855ed8bf2b44a68342c99eadf021c3c56ca4d
VinicioSales <vinicio.santtana@gmail.com>
2023-11-08 11:02:41 -0300

    feat: Criado auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from './guards/auth.guard';
+import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
 import { EsqueciSenhaComponent } from './components/esqueci-senha/esqueci-senha.component';
-import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  {path:'home', component: HomeComponent},
+  {path:'home', component: HomeComponent, canActivate: [AuthGuard]},
   {path:'login', component: LoginComponent},
   {path:'registro', component: RegistroComponent},
   {path:'esqueci-senha', component: EsqueciSenhaComponent},
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -10,6 +10,9 @@ export class AuthService {
 
   constructor(private http: HttpClient) {}
 
+  private autenticado = false;
+
+
   //NOTE - login
   login(email: string, senha: string): Observable<any> {
     return this.http.post(`${urlBackend}/login`, { email, senha });
@@ -18,5 +21,25 @@ export class AuthService {
   //NOTE recuperarSenha
   recuperarSenha(email: string): Observable<any> {
     return this.http.post(`${urlBackend}/recuperar-senha`, { email });
+  }
+
+  //NOTE - logar
+  logar() {
+    this.autenticado = true;
+    localStorage.setItem('autenticado', 'true'); // Armazene a autenticação no localStorage
+  }
+
+  // NOTE logout
+  logout() {
+    this.autenticado = false;
+    localStorage.removeItem('autenticado'); // Remova a autenticação do sessionStorage
+  }
+
+  //NOTE - checarAutenticacao
+  checarAutenticacao(): boolean {
+    if (sessionStorage.getItem('autenticado')) {
+      return true;
+    }
+    return this.autenticado;
   }
 }
