8d039d95f36f318fc2789c430f646859a1de0013
VinicioSales <vinicio.santtana@gmail.com>
2023-11-08 09:49:39 -0300

    feat: Add testes para serviços auth e imagem

--- src/app/services/auth/auth.service.spec.ts
+++ src/app/services/auth/auth.service.spec.ts
@@ -1,18 +1,69 @@
 import { TestBed } from '@angular/core/testing';
 import { AuthService } from './auth.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { urlBackend } from 'src/app/services/static';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule], // Adicione esta linha
+      imports: [HttpClientTestingModule],
+      providers: [AuthService]
     });
     service = TestBed.inject(AuthService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify(); // Verifica se não há requisições pendentes.
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  //SECTION - login
+  describe('login', () => {
+
+    //NOTE - deve reotornar um Observable<any>
+    it('deve reotornar um Observable<any>', () => {
+      const dummyResponse = { token: '12345' };
+      const email = 'test@example.com';
+      const senha = '123456';
+
+      service.login(email, senha).subscribe(response => {
+        expect(response).toEqual(dummyResponse);
+      });
+
+      const req = httpMock.expectOne(`${urlBackend}/login`);
+      expect(req.request.method).toBe('POST');
+      expect(req.request.body).toEqual({ email, senha });
+      req.flush(dummyResponse);
+    });
+  });
+  //!SECTION
+
+
+
+  //SECTION - recuperarSenha
+  describe('recuperarSenha', () => {
+
+    //NOTE - deve reotornar um Observable<any>
+    it('deve retornar um Observable<any>', () => {
+      const dummyResponse = { message: 'Instruções de recuperação enviadas.' };
+      const email = 'test@example.com';
+
+      service.recuperarSenha(email).subscribe(response => {
+        expect(response).toEqual(dummyResponse);
+      });
+
+      const req = httpMock.expectOne(`${urlBackend}/recuperar-senha`);
+      expect(req.request.method).toBe('POST');
+      expect(req.request.body).toEqual({ email });
+      req.flush(dummyResponse);
+    });
+  });
+  //!SECTION
 });
--- src/app/services/imagem.service.spec.ts
+++ src/app/services/imagem.service.spec.ts
@@ -1,16 +1,86 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ImagemService } from './imagem.service';
+import { TemaService } from './tema.service';
 
-describe('ImagemService', () => {
+fdescribe('ImagemService', () => {
   let service: ImagemService;
+  let temaService: TemaService;
+  let temaServiceSpy: jasmine.SpyObj<TemaService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    temaServiceSpy = jasmine.createSpyObj('TemaService', [], { 'temaEscuroLigado': true }) // A propriedade precisa ser acessível
+    
+    TestBed.configureTestingModule({
+      providers: [
+        ImagemService,
+        { provide: TemaService, useValue: temaServiceSpy }
+      ]
+    });
+    
     service = TestBed.inject(ImagemService);
+    temaService = TestBed.inject(TemaService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  // SECTION - atualizarImg
+  describe('atualizarImg', () => {
+
+    // NOTE - deve retornar imgTemaClaro quando temaEscuroLigado é falso
+    it('deve retornar imgTemaClaro quando temaEscuroLigado é falso', () => {
+      const imgClara = 'claro.png';
+      const imgEscura = 'escuro.png';
+      Object.defineProperty(temaServiceSpy, 'temaEscuroLigado', { get: () => false });
+      
+      expect(service.atualizarImg(imgClara, imgEscura)).toEqual(imgClara);
+    });
+
+    // NOTE - deve retornar imgTemaEscuro quando temaEscuroLigado é verdadeiro
+    it('deve retornar imgTemaEscuro quando temaEscuroLigado é verdadeiro', () => {
+      const imgClara = 'claro.png';
+      const imgEscura = 'escuro.png';
+      Object.defineProperty(temaServiceSpy, 'temaEscuroLigado', { get: () => true });
+      
+      expect(service.atualizarImg(imgClara, imgEscura)).toEqual(imgEscura);
+    });
   });
+  //!SECTION
+
+  // SECTION - getExitHover
+  describe('getExitHover', () => {
+
+    // NOTE - deve retornar o caminho correto para a imagem de hover de saída
+    it('deve retornar o caminho correto para a imagem de hover de saída', () => {
+      expect(service.getExitHover()).toEqual('assets/img/exit-hover.png');
+    });
+  });
+  //!SECTION
+
+  // SECTION - getRemoverHoverImg
+  describe('getRemoverHoverImg', () => {
+
+    // NOTE - deve retornar o caminho correto para a imagem de hover do botão remover
+    it('deve retornar o caminho correto para a imagem de hover do botão remover', () => {
+      expect(service.getRemoverHoverImg()).toEqual('assets/img/remover-hover.png');
+    });
+  });
+  //!SECTION
+
+  // SECTION - getEditarHoverImg
+  describe('getEditarHoverImg', () => {
+
+    // NOTE - deve retornar o caminho correto para a imagem de hover do botão editar
+    it('deve retornar o caminho correto para a imagem de hover do botão editar', () => {
+      expect(service.getEditarHoverImg()).toEqual('assets/img/editar-hover.png');
+    });
+  });
+  //!SECTION
+
+  // SECTION - getAjudaHoverImg
+  describe('getAjudaHoverImg', () => {
+
+    // NOTE - deve retornar o caminho correto para a imagem de hover do botão ajuda
+    it('deve retornar o caminho correto para a imagem de hover do botão ajuda', () => {
+      expect(service.getAjudaHoverImg()).toEqual('assets/img/ajuda-hover.png');
+    });
+  });
+  //!SECTION
 });
