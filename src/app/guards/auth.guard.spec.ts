b6433fde40a05f8d452bff875888f85c6b6f5b76
VinicioSales <vinicio.santtana@gmail.com>
2023-11-09 11:02:52 -0300

    feat: add testes para auth guard

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -1,30 +1,49 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 import { AuthGuard } from './auth.guard';
-import { AuthService } from 'src/app/services/auth/auth.service';
+import { AuthService } from '../services/auth/auth.service';
 
 describe('AuthGuard', () => {
-  let guard: AuthGuard;
-  
+  let authGuard: AuthGuard;
+  let authService: jasmine.SpyObj<AuthService>;
+  let router: jasmine.SpyObj<Router>;
+
   beforeEach(() => {
+    const authServiceSpy = jasmine.createSpyObj('AuthService', ['isLoggedIn']);
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
     TestBed.configureTestingModule({
-      imports: [
-        HttpClientTestingModule, // Fornece um ambiente de teste para HttpClient
-        RouterTestingModule // Fornece um ambiente de teste para o Router
-      ],
       providers: [
         AuthGuard,
-        AuthService // Garante que o AuthService está disponível para o AuthGuard
+        { provide: AuthService, useValue: authServiceSpy },
+        { provide: Router, useValue: routerSpy }
       ]
     });
+
+    authGuard = TestBed.inject(AuthGuard);
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
+  });
+
+  // SECTION - canActivate
+  describe('canActivate', () => {
     
-    guard = TestBed.inject(AuthGuard);
+    // NOTE - deve permitir a ativação da rota quando o usuário está logado
+    it('deve permitir a ativação da rota quando o usuário está logado', () => {
+      authService.isLoggedIn.and.returnValue(true);
+      const result = authGuard.canActivate();
+      expect(result).toBeTrue();
+      expect(router.navigate).not.toHaveBeenCalled();
+    });
+
+    // NOTE - deve redirecionar para login quando o usuário não está logado
+    it('deve redirecionar para login quando o usuário não está logado', () => {
+      authService.isLoggedIn.and.returnValue(false);
+      const result = authGuard.canActivate();
+      expect(result).toBeFalse();
+      expect(router.navigate).toHaveBeenCalledWith(['/login']);
+    });
+
   });
 
-  it('should be created', () => {
-    expect(guard).toBeTruthy();
-  });
-
-  // Adicione seus testes aqui
 });
