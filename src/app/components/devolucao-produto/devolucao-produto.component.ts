b065a73ab1121a2595bf3b3ef15812c74cc26926
0victorsales <victorhsales0@gmail.com>
2023-11-23 17:49:37 -0300

    ajuste no css da pagina de devolucao de produtos e inicio do script

--- src/app/components/devolucao-produto/devolucao-produto.component.ts
+++ src/app/components/devolucao-produto/devolucao-produto.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { NgModel } from '@angular/forms';
 
 
+
 @Component({
   selector: 'app-devolucao-produto',
   templateUrl: './devolucao-produto.component.html',
@@ -13,5 +14,25 @@ import { NgModel } from '@angular/forms';
 
 
 export class DevolucaoProdutoComponent {
+
+  produto = [
+    {
+      codSolicitacao: '001',
+      codProduto: 'A123',
+      produto: 'Produto 1',
+      quantidade: 10,
+      uniMedida: 'Un',
+      centroCusto: 'CC1'
+    },
+    
+  ];
+
+  linhas: any[] = [
+    { codSolicitacao: '123456', /* Outros campos da primeira linha */ }
+  ];
+
+  adicionarLinha(): void {
+    this.linhas.push({ codSolicitacao: 'NovoCodigo', /* Outros campos da nova linha */ });
+  }
  
 }
